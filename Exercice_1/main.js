b446244bbf5f333f199e1a2d55548c59ff511c64
Berthelot Léane <leane.berthelot@etu.u-bordeaux.fr>
2022-12-06 16:05:24 +0100

    refactor

--- Exercice_1/main.js
+++ Exercice_1/main.js
@@ -1,12 +1,14 @@
-const addElement = (elementName, elementContent, elementUrl, ParentElement) => {
+const addElement = (elementName, parentElement, elementContent = "") => {
   const element = document.createElement(elementName);
-  if (null != elementContent) {
-    element.textContent = elementContent;
-  }
-  if (null != elementUrl) {
-    element.src = elementUrl;
-  }
-  ParentElement.appendChild(element);
+  element.textContent = elementContent;
+  parentElement.appendChild(element);
+  return element;
+};
+
+const addImg = (elementName, parentElement, elementUrl) => {
+  const element = document.createElement(elementName);
+  element.src = elementUrl;
+  parentElement.appendChild(element);
   return element;
 };
 
@@ -17,28 +19,32 @@ const fetchRandomDrink = async () => {
   return await reponseCocktail.json();
 };
 
+const clearElement = (element) => {
+  element.innerHTML = "";
+};
 const showRandomCocktail = async () => {
-  div.innerHTML = "";
-
   const reponse = await fetchRandomDrink();
   const randomDrink = reponse.drinks[0];
 
-  addElement("h1", randomDrink.strDrink, null, div);
-  addElement("p", "Catégorie : " + randomDrink.strCategory, null, div);
-  addElement("p", "Instructions : " + randomDrink.strInstructions, null, div);
+  addElement("h1", div, randomDrink.strDrink);
+  addElement("p", div, "Catégorie : " + randomDrink.strCategory);
+  addElement("p", div, "Instructions : " + randomDrink.strInstructions);
 
-  const ul = addElement("ul", null, null, div);
+  const ul = addElement("ul", div);
 
   let i = 1;
   while (null != randomDrink["strIngredient" + i] || i > 15) {
-    addElement("li", randomDrink["strIngredient" + i], null, ul);
+    addElement("li", ul, randomDrink["strIngredient" + i]);
     i++;
   }
 
-  addElement("img", null, randomDrink.strDrinkThumb, div);
+  addImg("img", div, randomDrink.strDrinkThumb);
 };
 
 const root = document.getElementById("root");
-const button = addElement("button", "Click me", null, root);
-const div = addElement("div", null, null, root);
-button.addEventListener("click", showRandomCocktail);
+const button = addElement("button", root, "Click me");
+const div = addElement("div", root);
+button.addEventListener("click", () => {
+  clearElement(div);
+  showRandomCocktail();
+});
